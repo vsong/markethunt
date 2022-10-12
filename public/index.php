40c222770626191631016acb84fee2830c2d592f
Victor Song <victor.xpsong@gmail.com>
2022-10-12 12:22:11 -0700

    Get Slim framework up and running

--- public/index.php
+++ public/index.php
@@ -1,8 +1,21 @@
 <?php
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../config');
 $dotenv->safeLoad();
 
-echo 'hello world ';
-echo $_ENV['FOO'];
+$app = AppFactory::create();
+$container = $app->getContainer();
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+
+$app->get('/test/{id}', function (Request $request, Response $response, $args) {
+    $id = $args['id'];
+    return $response->withJson(array('id' => $id));
+});
+
+$app->run();
