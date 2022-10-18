5a360081f45195550b7fdf0ea6a9765720d19527
Victor Song <victor.xpsong@gmail.com>
2022-10-17 20:57:44 -0700

    Get item headers endpoint working

--- src/Controller/MarketInfoController.php
+++ src/Controller/MarketInfoController.php
@@ -8,19 +8,23 @@ use Slim\Http\Response as Response;
 
 class AsdfController
 {
-    private $datetime;
+    private \PDO $db;
 
     public function __construct(ContainerInterface $container)
     {
-        $this->datetime = $container->get('date');
+        $this->db = $container->get('db');
     }
 
     public function Asdf(Request $request, Response $response, $args) {
-        $id = $args['id'];
+        $id = intval($args['id']);
+
+        $stmt = $this->db->prepare('SELECT * FROM v_latest_price WHERE item_id = :item_id');
+        $stmt->execute(['item_id' => $id]);
+        $metadata = $stmt->fetchAll();
 
         $data = [
             'id' => $id,
-            'date' => $this->datetime
+            'metadata' => $metadata
         ];
 
         return $response->withJson($data);
---
+++ src/Model/MarketDatapoint.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model;
+
+use App\Util\DateUtils;
+use DateTime;
+use JsonSerializable;
+
+class ItemMarketDatapoint implements JsonSerializable
+{
+    public int $itemId;
+    public DateTime $date;
+    public int $price;
+    public float $sbPrice;
+    public int $volume;
+
+    public function __construct(int $itemId, DateTime $date, int $price, float $sbPrice, int $volume) {
+        $this->itemId = $itemId;
+        $this->date = $date;
+        $this->price = $price;
+        $this->sbPrice = $sbPrice;
+        $this->volume = $volume;
+    }
+
+    public function jsonSerialize() {
+        return [
+            'date' => DateUtils::DateTimeToUtcIsoDate($this->date),
+            'price' => $this->price,
+            'sb_price' => $this->sbPrice,
+            'volume' => $this->volume
+        ];
+    }
+}
