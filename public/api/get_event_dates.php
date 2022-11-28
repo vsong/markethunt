1f48274579fd6ad9f06e9711c5500cafd6e3425e
Victor Song <victor.xpsong@gmail.com>
2022-11-27 21:07:29 -0800

    Migration cutoff

--- public/api/get_event_dates.php
+++ public/api/get_event_dates.php
@@ -1,14 +1,15 @@
 <?php
 require_once($_SERVER['APPDIR'] . "/config/setup.php");
-require_once($_SERVER['APPDIR'] . "/model/ItemModel.php");
 
 ob_start();
-
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-$event_data = file_get_contents($_SERVER['APPDIR'] . "/templates/chart/event_dates.json");
+$newUrl = "https://$API_HOST/events";
+$json = json_encode([
+    'message' => "Moved to $newUrl",
+    'success' => false
+]);
 
-echo $event_data;
-
-ob_end_flush();
+echo $json;
+die();
--- public/api/stock_data/getjson.php
+++ public/api/stock_data/getjson.php
@@ -1,29 +1,14 @@
 <?php
 require_once($_SERVER['APPDIR'] . "/config/setup.php");
-require_once($_SERVER['APPDIR'] . "/model/ItemModel.php");
 
 ob_start();
-
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-if (filter_input(INPUT_GET, 'item_id', FILTER_VALIDATE_INT)) {
-    $current_item_id = (int)$_GET['item_id'];
-    if (!isValidItemId($current_item_id)) {
-        echo '{"success":false}';
-        die();
-    }
-} else {
-    echo '{"success":false}';
-    die();
-}
+$newUrl = "https://$API_HOST/items/{$_GET['item_id']}";
 
-$stock_data = [
-    "success" => true,
-    "item_id" => $current_item_id,
-    "data" => getItemChartData($current_item_id),
-    "latest_sb_data" => getLatestItemPrice(114)[0],
-];
-
-echo json_encode($stock_data);
-ob_end_flush();
+echo json_encode([
+    'message' => "Moved to $newUrl",
+    'success' => false
+]);
+die();
